1cfccfc38ffa1b001df25007cabaadfb0c67e4ee
Rakanta Rifky Eka Indra Putra <rakanta.rifky@gmail.com>
2024-01-28 18:27:53 +0700

    Update main.ino
    Update Script

--- main.ino
+++ main.ino
@@ -8,12 +8,24 @@ CD74HC4067 muxDigital {
   {0, 1, 2, 3 } // Address pins S0, S1, S2, S3
 };
 
-CD74HC4067 muxAnalog {
-  A5,       // Analog input pin
-  {10, 11, 12, 13} // Address pins S0, S1, S2, S3
+CD74HC4067 muxDigital1 {
+  5,       // Digital input pin
+  {0, 1, 2, 3 } // Address pins S0, S1, S2, S3
 };
 
-//pin available 4,5,6,7,8
+CD74HC4067 muxAnalog {
+  A5,       // Analog input pin
+  {12, 13, 14, 15} // Address pins S0, S1, S2, S3
+};
+
+CD74HC4067 muxAnalog2 {
+  A4,       // Analog input pin
+  {12, 13, 14, 15} // Address pins S0, S1, S2, S3
+};
+
+//pin available 6,7,8,9,10,11 , 6,7,8,9 will be use for rotary . 10,11 will be used by sensor
+//analog available A1,A2,A3
+
 
 // Note Release
 BEGIN_CS_NAMESPACE
@@ -83,9 +95,30 @@ MyNoteButton button1[] {
   { muxDigital.pin(15),{MIDI_CC::Breath_Controller, Channel_1},0x7F },
 };
 
+MyNoteButton button2[] {
+  { muxDigital1.pin(0),{MIDI_Notes::C(4), Channel_1},0x7F }, 
+  { muxDigital1.pin(1),{MIDI_Notes::D(4), Channel_1},0x7F },
+  { muxDigital1.pin(2),{MIDI_Notes::E(4), Channel_1},0x7F },
+  { muxDigital1.pin(3),{MIDI_Notes::F_(4), Channel_1},0x7F },
+  { muxDigital1.pin(4),{MIDI_Notes::G(4), Channel_1},0x7F },
+  { muxDigital1.pin(5),{MIDI_Notes::A(4), Channel_1},0x7F },
+  { muxDigital1.pin(6),{MIDI_Notes::B(4), Channel_1},0x7F },
+  { muxDigital1.pin(7),{MIDI_Notes::C(5), Channel_1},0x7F },
+  { muxDigital1.pin(8),{MIDI_Notes::D(5), Channel_1},0x7F },
+  { muxDigital1.pin(9),{MIDI_Notes::E(5), Channel_1},0x7F },
+  { muxDigital1.pin(8),{MIDI_CC::Portamento, Channel_1},0x7F },
+  { muxDigital1.pin(9),{MIDI_CC::Portamento_Control, Channel_1},0x7F },
+  { muxDigital1.pin(10),{MIDI_CC::Effects_1, Channel_1},0x7F },
+  { muxDigital1.pin(11),{MIDI_CC::Effects_2, Channel_1},0x7F },
+  { muxDigital1.pin(12),{MIDI_CC::Effects_3, Channel_1},0x7F },
+  { muxDigital1.pin(13),{MIDI_CC::Effects_4, Channel_1},0x7F },
+  { muxDigital1.pin(14),{MIDI_CC::Effects_5, Channel_1},0x7F },
+  { muxDigital1.pin(15),{MIDI_CC::Breath_Controller, Channel_1},0x7F },
+};
+
 // Create an array of CCPotentiometer objects that send out MIDI Control Change 
 // messages when you turn the potentiometers connected to the 8 inputs of the mux.
-CCPotentiometer PotentiometersCC[] {
+CCPotentiometer Analog1[] {
   { muxAnalog.pin(0), { MIDI_CC::Channel_Volume, Channel_1 } },
   { muxAnalog.pin(1), { MIDI_CC::Effects_1, Channel_1 } },
   { muxAnalog.pin(2), { MIDI_CC::Effects_2, Channel_2 } },
@@ -104,19 +137,39 @@ CCPotentiometer PotentiometersCC[] {
   { muxAnalog.pin(15), { MIDI_CC::Effects_5, Channel_8 } },
 };
 
+CCPotentiometer Analog2[] {
+  { muxAnalog2.pin(0), { MIDI_CC::Channel_Volume, Channel_1 } },
+  { muxAnalog2.pin(1), { MIDI_CC::Effects_1, Channel_1 } },
+  { muxAnalog2.pin(2), { MIDI_CC::Effects_2, Channel_2 } },
+  { muxAnalog2.pin(3), { MIDI_CC::Effects_3, Channel_3 } },
+  { muxAnalog2.pin(4), { MIDI_CC::Effects_4, Channel_5 } },
+  { muxAnalog2.pin(5), { MIDI_CC::Effects_5, Channel_6 } },
+  { muxAnalog2.pin(6), { MIDI_CC::Channel_Volume, Channel_7 } },
+  { muxAnalog2.pin(7), { MIDI_CC::Channel_Volume, Channel_8 } },
+  { muxAnalog2.pin(8), { MIDI_CC::Effects_5, Channel_8 } },
+  { muxAnalog2.pin(9), { MIDI_CC::Effects_5, Channel_8 } },
+  { muxAnalog2.pin(10), { MIDI_CC::Effects_5, Channel_8 } },
+  { muxAnalog2.pin(11), { MIDI_CC::Effects_5, Channel_8 } },
+  { muxAnalog2.pin(12), { MIDI_CC::Effects_5, Channel_8 } },
+  { muxAnalog2.pin(13), { MIDI_CC::Effects_5, Channel_8 } },
+  { muxAnalog2.pin(14), { MIDI_CC::Effects_5, Channel_8 } },
+  { muxAnalog2.pin(15), { MIDI_CC::Effects_5, Channel_8 } },
+};
+
 
 // Instantiate a CCAbsoluteEncoder object
 CCAbsoluteEncoder enc {
-  {8, 9},       // pins
+  {6, 7},       // pins
   MIDI_CC::Portamento, // MIDI address (CC number + optional channel)
   5,            // optional multiplier if the control isn't fast enough
 };
 CCAbsoluteEncoder enc1 {
-  {6, 7},       // pins
+  {8, 9},       // pins
   MIDI_CC::Expression_Controller, // MIDI address (CC number + optional channel)
   5,            // optional multiplier if the control isn't fast enough
 };
 
+//For sensor
 
 //Pitch bend
 PBPotentiometer potentiometer {
